e6439b092c9d9adc1a46cda54e366a80417ac5ed
locson <hualocson@gmail.com>
2023-09-30 20:38:40 +0700

    UPDATE: add auth middleware

---
+++ src/apis/middlewares/adminAuthorization.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import configs from "@/configs/vars";
+import ApiError from "@/lib/api-error";
+
+export default function adminAuthorization(adminRequiredRank) {
+  return async (req, res, next) => {
+    try {
+      const jwtToken = req.cookies["admin-auth"];
+
+      if (!jwtToken) {
+        next(new ApiError(403, "Access unauthorized. Please login."));
+      }
+
+      const payload = jwt.verify(jwtToken, configs.jwtSecret);
+
+      if (!payload.adminRank) {
+        next(
+          new ApiError(
+            403,
+            "Access unauthorized. Please login with admin account."
+          )
+        );
+      }
+
+      if (payload.adminRank < adminRequiredRank) {
+        next(
+          new ApiError(
+            403,
+            "Not enough privilege to execute this request. Contact admin."
+          )
+        );
+      }
+
+      next();
+    } catch (error) {
+      next(new ApiError(403, "Access unauthorized. Please login."));
+    }
+  };
+}
--- src/apis/routes/assets.router.js
+++ src/apis/routes/assets.router.js
@@ -1,9 +1,11 @@
 import assetsController from "../controllers/assets.controller";
+import adminAuthorization from "../middlewares/adminAuthorization";
 import uploadFileMiddleware from "../middlewares/multer.middleware";
 const assetsRoutes = (router) => {
   // upload new asset
   router.post(
     "/assets",
+    adminAuthorization(1),
     uploadFileMiddleware.single("image"),
     assetsController.uploadImage
   );
