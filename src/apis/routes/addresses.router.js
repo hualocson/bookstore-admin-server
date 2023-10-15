435c1f94ba6b1da104d336652906b5335d00b591
LAPTOP-182PLMJF\PC <20110615@student.hcmute.edu.vn>
2023-10-15 14:10:26 +0700

    add get for admin

---
+++ src/apis/routes/addresses.router.js
@@ -0,0 +1,18 @@
+import { body, param } from "express-validator";
+import addressesController from "../controllers/addresses.controller";
+import adminAuthorization from "../middlewares/auth";
+/**
+ *
+ * @param {import('express').Router} router
+ */
+
+const addressesRoutes = (router) => {
+    router.get(
+      "/addresses",
+      adminAuthorization(1),
+      addressesController.getAllAddresses
+    );
+    
+};
+  
+  export default addressesRoutes;
