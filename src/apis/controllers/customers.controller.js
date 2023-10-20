4c61c23875f42e3ae4a209513701c99ab1039bf1
locson <hualocson@gmail.com>
2023-10-20 10:13:20 +0700

    UPDATE: format code, fix returning field after update

--- src/apis/controllers/customers.controller.js
+++ src/apis/controllers/customers.controller.js
@@ -1,41 +1,42 @@
 import { controllerWrapper } from "@/lib/controller.wrapper";
-import { CustomerStatus } from "@/lib/constants";
 
 const customersController = {
-    
-    getAllCustomers: controllerWrapper(
-      async (_, res, { errorResponse, successResponse, sql }) => {
-        const customers = await sql`
+  getAllCustomers: controllerWrapper(
+    async (_, res, { errorResponse, successResponse, sql }) => {
+      const customers = await sql`
           SELECT id, email, status
           FROM customers
         `;
-  
-        return successResponse(
-          { customers },
-          "Get all customers successfully",
-          200
-        );
-      }
-    ),
-  
-    updateCustomerStatus: controllerWrapper(
+
+      return successResponse(
+        { customers },
+        "Get all customers successfully",
+        200
+      );
+    }
+  ),
+
+  updateCustomerStatus: controllerWrapper(
     async (req, _, { errorResponse, successResponse, sql }) => {
       const { status } = req.body;
       const { id } = req.params;
 
       const [existingStatus] = await sql`
-        SELECT status FROM customers WHERE id = ${id}
+        SELECT status FROM customers WHERE id = ${id} AND deleted_at IS NULL
       `;
 
       if (!existingStatus) {
-        return errorResponse(`Customer with id ${id} not found`, 404);
+        return errorResponse(
+          `Customer with id ${id} not found or deleted`,
+          404
+        );
       }
 
       const [customer] = await sql`
         UPDATE customers
         SET status = ${status}
         WHERE id = ${id}
-        RETURNING id, emailstatus
+        RETURNING id, email, status
       `;
 
       return successResponse(
@@ -46,5 +47,5 @@ const customersController = {
     }
   ),
 };
-  
+
 export default customersController;
