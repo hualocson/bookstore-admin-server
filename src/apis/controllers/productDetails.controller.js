0b70a049024999ee28ce2c15b1462c667cb94d73
locson <hualocson@gmail.com>
2023-11-17 18:58:52 +0700

    UPDATE: modify product detail

--- src/apis/controllers/productDetails.controller.js
+++ src/apis/controllers/productDetails.controller.js
@@ -1,38 +1,37 @@
 import { controllerWrapper } from "@/lib/controller.wrapper";
 
-
 const productDetailsController = {
-    //create productDetails
-    createProductDetails: controllerWrapper(
-        async (req, _, {errorResponse, successResponse, sql }) => {
-            const {id ,pages, author, publisher, publicationDate } = req.body;
-            //Check if product id is exist
-            const [product] = await sql`
+  // create productDetails
+  createProductDetails: controllerWrapper(
+    async (req, _, { errorResponse, successResponse, sql }) => {
+      const { id, pages, author, publisher, publicationDate } = req.body;
+      // Check if product id is exist
+      const [product] = await sql`
             SELECT id FROM products WHERE id = ${id}
             `;
-            if (!product) {
-                return errorResponse(`Product with id ${id} not found`, 404);
-            }
-            //create new productDetails
-            const [newProductDetails] = await sql`
+      if (!product) {
+        return errorResponse(`Product with id ${id} not found`, 404);
+      }
+      // create new productDetails
+      const [newProductDetails] = await sql`
             INSERT INTO product_details
             (id,pages, author, publisher, publication_date)
             VALUES
             (${id},${pages}, ${author}, ${publisher}, ${publicationDate})
             RETURNING id, pages, author, publisher, publication_date
             `;
-            return successResponse(
-                { newProductDetails },
-                "Create new product details successfully",
-                200
-            );
-        }
-    ),
+      return successResponse(
+        { newProductDetails },
+        "Create new product details successfully",
+        200
+      );
+    }
+  ),
 
-    // getAllProductDetails
-    getAllProductDetails: controllerWrapper(
-        async (req, _, { successResponse, sql }) => {
-            const productDetails = await sql`
+  //  getAllProductDetails
+  getAllProductDetails: controllerWrapper(
+    async (req, _, { successResponse, sql }) => {
+      const productDetails = await sql`
             SELECT
                 pd.id,
                 pd.pages,
@@ -46,26 +45,26 @@ const productDetailsController = {
             ORDER BY pd.created_at
             `;
 
-            return successResponse(
-                { productDetails },
-                "Get all product details successfully",
-                200
-            );
-        }
-    ),
+      return successResponse(
+        { productDetails },
+        "Get all product details successfully",
+        200
+      );
+    }
+  ),
 
-    // getProductDetailsById
-    getProductDetailsById: controllerWrapper(
-        async (req, _, { successResponse, sql }) => {
-            const { id } = req.params;
-            //Check if product id is exist
-            const [product] = await sql`
+  //  getProductDetailsById
+  getProductDetailsById: controllerWrapper(
+    async (req, _, { successResponse, errorResponse, sql }) => {
+      const { id } = req.params;
+      // Check if product id is exist
+      const [product] = await sql`
             SELECT id FROM products WHERE id = ${id}
             `;
-            if (!product) {
-                return errorResponse(`Product with id ${id} not found`, 404);
-            }
-            const [productDetails] = await sql`
+      if (!product) {
+        return errorResponse(`Product with id ${id} not found`, 404);
+      }
+      const [productDetails] = await sql`
             SELECT
                 pd.id,
                 pd.pages,
@@ -79,72 +78,75 @@ const productDetailsController = {
             WHERE pd.id = ${id}
             `;
 
-            return successResponse(
-                { productDetails },
-                "Get product details by id successfully",
-                200
-            );
-        }
-    ),
-    
-    //Update productDetails
-    updateProductDetails: controllerWrapper(
-        async (req, _, { errorResponse,successResponse, sql }) => {
-            const { pages, author, publisher, publicationDate } = req.body;
-            const { id } = req.params;
-            //Check if product id is exist
-            const [product] = await sql`
+      return successResponse(
+        { productDetails },
+        "Get product details by id successfully",
+        200
+      );
+    }
+  ),
+
+  // Update productDetails
+  updateProductDetails: controllerWrapper(
+    async (req, _, { errorResponse, successResponse, sql }) => {
+      const { pages, author, publisher, publicationDate } = req.body;
+      const { id } = req.params;
+      // Check if product id is exist
+      const [product] = await sql`
             SELECT id,deleted_at,status FROM products WHERE id = ${id}
             `;
-            if (!product) {
-                return errorResponse(`Product with id ${id} not found`, 404);
-            }
-            const [existingProductDetails] = await sql`SELECT id,pages,author FROM product_details WHERE id = ${id}
+      if (!product) {
+        return errorResponse(`Product with id ${id} not found`, 404);
+      }
+      const [existingProductDetails] =
+        await sql`SELECT id,pages,author FROM product_details WHERE id = ${id}
             `;
-            if (!existingProductDetails) {
-                return errorResponse(`Product details with product id ${id} not found`, 404);
-            }
-            const [productDetails] = await sql`
-            UPDATE product_details
-            SET
-                pages = ${pages},
-                author = ${author},
-                publisher = ${publisher},
-                publication_date = ${publicationDate}
-            WHERE id = ${id}
-            RETURNING id, pages, author, publisher, publication_date
-            `;
-
-            return successResponse(
-                { productDetails },
-                "Update product details successfully",
-                200
-            );
-        }
-    ),
-    //delete productDetails
-    deleteProductDetails: controllerWrapper(
-        async (req, _, { successResponse, sql }) => {
-            const { id } = req.params;
-            //Check if product id is exist
-            const [product] = await sql`
+      if (!existingProductDetails) {
+        await sql`
+      INSERT INTO product_details
+      (id,pages, author, publisher, publication_date)
+      VALUES
+      (${id},${pages}, ${author}, ${publisher}, ${publicationDate})
+      RETURNING id, pages, author, publisher, publication_date
+      `;
+      } else {
+        await sql`
+        UPDATE product_details
+        SET
+            pages = ${pages},
+            author = ${author},
+            publisher = ${publisher},
+            publication_date = ${publicationDate}
+        WHERE id = ${id}
+        RETURNING id, pages, author, publisher, publication_date
+        `;
+      }
+      return successResponse({}, "Update product details successfully", 200);
+    }
+  ),
+  // delete productDetails
+  deleteProductDetails: controllerWrapper(
+    async (req, _, { successResponse, errorResponse, sql }) => {
+      const { id } = req.params;
+      // Check if product id is exist
+      const [product] = await sql`
             SELECT id FROM products WHERE id = ${id}
             `;
-            if (!product) {
-                return errorResponse(`Product with id ${id} not found`, 404);
-            }
-            const [productDetails] = await sql`
+      if (!product) {
+        return errorResponse(`Product with id ${id} not found`, 404);
+      }
+      const [productDetails] = await sql`
             DELETE FROM product_details
             WHERE id = ${id}
             RETURNING id, pages, author, publisher, publication_date
             `;
 
-            return successResponse(
-                { productDetails },
-                "Delete product details successfully",
-                200
-            );
-        }
-    )
-}
+      return successResponse(
+        { productDetails },
+        "Delete product details successfully",
+        200
+      );
+    }
+  ),
+};
 export default productDetailsController;
